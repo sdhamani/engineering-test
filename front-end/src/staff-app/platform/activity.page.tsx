4e4cb26a24534cbceea3d199cb9d8940faea0381
sdhamani <sagar.dhamani1@gmail.com>
2021-07-14 00:22:47 +0530

    Fetched activities on activity page

--- front-end/src/staff-app/platform/activity.page.tsx
+++ front-end/src/staff-app/platform/activity.page.tsx
@@ -1,8 +1,16 @@
-import React from "react"
+import React, { useEffect } from "react"
 import styled from "styled-components"
 import { Spacing } from "shared/styles/styles"
+import { useApi } from "shared/hooks/use-api"
+import { Person } from "shared/models/person"
 
 export const ActivityPage: React.FC = () => {
+  const [getRolls, data, loadState] = useApi<{ students: Person[] }>({ url: "get-activities" })
+
+  useEffect(() => {
+    void getRolls()
+  }, [getRolls])
+
   return <S.Container>Activity Page</S.Container>
 }
 
